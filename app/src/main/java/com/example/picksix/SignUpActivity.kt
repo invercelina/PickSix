94b7e808ac699a7d5229495f86e5c02bf7a803a2
invercelina <invercelina@gmail.com>
2024-06-07 14:53:50 +0900

    내 생각에서는 회원가입 완료

--- app/src/main/java/com/example/picksix/SignUpActivity.kt
+++ app/src/main/java/com/example/picksix/SignUpActivity.kt
@@ -1,7 +1,9 @@
 package com.example.picksix
 
+import android.app.Activity
 import android.os.Bundle
-import android.provider.ContactsContract.CommonDataKinds.Nickname
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
@@ -25,6 +27,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
@@ -39,6 +42,8 @@ import io.github.jan.supabase.gotrue.providers.builtin.Email
 import io.github.jan.supabase.postgrest.Postgrest
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.put
 
 val supabase = createSupabaseClient(
     supabaseUrl = BuildConfig.supabaseUrl,
@@ -48,10 +53,13 @@ val supabase = createSupabaseClient(
     install(Auth)
 }
 
-suspend fun signUpNewUser(userMail:String, userPassword:String) {
+suspend fun signUpNewUser(userMail: String, userPassword: String, nickname: String) {
     supabase.auth.signUpWith(Email) {
         email = userMail
         password = userPassword
+        data = buildJsonObject {
+            put("nickname", nickname)
+        }
     }
 }
 
@@ -73,6 +81,7 @@ class SignUpActivity : ComponentActivity() {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            val context = LocalContext.current as? Activity
             Spacer(modifier = Modifier.height(80.dp))
             Text(text = "PickSix", fontSize = 30.sp, fontWeight = FontWeight.Bold)
             Spacer(modifier = Modifier.height(40.dp))
@@ -115,7 +124,7 @@ class SignUpActivity : ComponentActivity() {
                     visualTransformation = PasswordVisualTransformation(),
                 )
                 Text(
-                    text = "비밀번호는 ${maxChar}글자까지 가능합니다",
+                    text = "6자부터 ${maxChar}글자까지 가능합니다",
                     modifier = Modifier
                         .padding(horizontal = 35.dp)
                         .fillMaxWidth(),
@@ -129,7 +138,7 @@ class SignUpActivity : ComponentActivity() {
                     },
                 )
                 Text(
-                    text = "닉네임은 ${maxChar}글자까지 가능합니다",
+                    text = "${maxChar}글자까지 가능합니다",
                     modifier = Modifier
                         .padding(horizontal = 35.dp)
                         .fillMaxWidth(),
@@ -138,7 +147,37 @@ class SignUpActivity : ComponentActivity() {
                 Spacer(modifier = Modifier.height(30.dp))
                 Button(
                     onClick = {
-                        runBlocking { launch { signUpNewUser(email,password) } }
+                        runCatching {
+                            runBlocking {
+                                launch {
+                                    // 여기에 코루틴이 실행될 코드를 작성
+                                    signUpNewUser(email, password, nickname)
+                                }
+                            }
+                        }.onSuccess {
+                            // 회원가입 성공 시 수행할 작업
+                            val toast = Toast.makeText(context,"회원가입 완료",Toast.LENGTH_SHORT)
+                            toast.show()
+                            context?.finish()
+                        }.onFailure { e ->
+                            // 회원가입 실패 시 수행할 작업
+                            // 예외 처리
+                            Log.e("SignUpError", "회원가입 중 오류 발생: ${e.message}", e)
+                            // 적절한 오류 처리 작업 수행
+                            val message = when{
+                                e.message?.contains("Unable to validate email address: invalid format") == true -> "올바른 이메일 형식을 입력해주세요"
+                                e.message?.contains("Password should be at least 6 characters.") == true -> "비밀번호는 최소 6자리 이상 입력해주세요"
+                                e.message?.contains("Anonymous sign-ins are disabled") == true -> "이메일을 입력해주세요"
+                                e.message?.contains("User already registered") == true -> "이미 회원가입된 이메일입니다"
+                                else -> null
+                            }
+                            val toast = Toast.makeText(
+                                context,
+                                "회원가입 중 오류 발생: $message",
+                                Toast.LENGTH_SHORT
+                            )
+                            toast.show() // 토스트 메시지를 보여주는 show() 메소드 호출
+                        }
                     },
                     modifier = Modifier
                         .padding(horizontal = 30.dp)
